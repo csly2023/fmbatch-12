8a857fd4e6e2212a92a1086976921d6c5d0d2c1b
csly2023 <csly.2023@gmail.com>
2024-04-23 20:50:30 +0600

    operator

--- app/classes/HelloWorld.php
+++ app/classes/HelloWorld.php
@@ -7,14 +7,37 @@ namespace App\classes;
 class HelloWorld
 {
     public $message;
+    public $firstName="BITM";
+    public $secondName="BASIS";
+
+    public $firstNumber;
+    public $secondNumber;
+    public $thirdNumber;
     public function __construct()
     {
         $this->message= "Hello World Farhad";
+        $this->firstNumber=20;
+        $this->secondNumber=30;
 
     }
     public function index()
     {
-        echo $this->message;
+//        echo $this->firstName.' &nbsp &nbsp &nbsp'.$this->secondName;
+
+        echo $this->thirdNumber=$this->firstNumber+$this->secondNumber;
+        echo '<br/>';
+
+        echo $this->thirdNumber=$this->firstNumber-$this->secondNumber;
+        echo '<br/>';
+
+        echo $this->thirdNumber=$this->firstNumber*$this->secondNumber;
+        echo '<br/>';
+
+        echo $this->thirdNumber=$this->firstNumber/$this->secondNumber;
+        echo '<br/>';
+
+        echo $this->thirdNumber=$this->firstNumber%$this->secondNumber;
+        echo '<br/>';
     }
 
 }
--- index.php
+++ index.php
@@ -6,4 +6,9 @@ use App\classes\HelloWorld;
 $helloWorld= new HelloWorld();
 $helloWorld->index();
 
+
+
+
+//Notes:
 //For vendor file creation comment= composer dump-autoload
+//gettype ()= method for php data type check
