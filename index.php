9095bac7df5e38e2c43dcfcf40c105a5ba0adeea
csly2023 <csly.2023@gmail.com>
2024-04-28 20:33:51 +0600

    multi array

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 require_once "vendor/autoload.php";
 use App\classes\HelloWorld;
+use App\classes\Home;
 
-$helloWorld= new HelloWorld();
-$helloWorld->index();
+//$helloWorld= new HelloWorld();
+//$helloWorld->index();
+
+$home= new Home();
+$home->index();
 
 
 
