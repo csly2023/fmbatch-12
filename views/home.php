6fd003544e457e2b779e58b854443fcb95e8eee7
csly2023 <csly.2023@gmail.com>
2024-05-09 18:07:52 +0600

    exam array files added as zip

--- views/home.php
+++ views/home.php
@@ -23,66 +23,10 @@
 
 <br><br><br>
 
-<input type="datetime-local">
-<br>
-<textarea></textarea>
 
-
-<select name="" id="">
-    <option>dhaka</option>
-    <option>gazipur</option>
-    <option>narayangonj</option>
-</select>
-
-
-<input type="radio">Male
-
-<input type="checkbox">Bangla
-
-<ul>
-    <li>banana
-        <ul>
-            <li>Banana one</li>
-            <li>Banana two</li>
-            <li>Banana three</li>
-            <li>Banana four</li>
-        </ul>
-
-    </li>
-    <li>jackfruit</li>
-    <li>apple</li>
-</ul>
-
-<ol>
-    <li>Banana</li>
-    <li>Jackfruit
-        <ul>
-            <li>Banana one</li>
-            <li>Banana two</li>
-            <li>Banana three</li>
-            <li>Banana four</li>
-        </ul>
-    </li>
-    <li>Apple</li>
-</ol>
-
-
-<a href="http://www.google.com" target="_blank">Click Me</a>
-
-<h1>This is heading</h1>
-<h2>This is heading</h2>
-<h3>This is heading</h3>
-<h4>This is heading</h4>
-<h5>This is heading</h5>
-<h6>This is heading</h6>
-
-<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    Assumenda consectetur culpa distinctio, illo incidunt iste laborum
-    laudantium magni minima necessitatibus officia officiis provident quam
-    quo ratione rerum sed voluptate voluptates.</p>
-
-<img src="../assets/img/2.jpg" alt="not found">
-<img src="./assets/img/2.jpg" alt="ok" width="300" height="200">
+<!---->
+<!--<img src="../assets/img/2.jpg" alt="not found">-->
+<!--<img src="./assets/img/2.jpg" alt="ok" width="300" height="200">-->
 
 
 </body>
