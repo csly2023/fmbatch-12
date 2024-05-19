42d481aad090bd8f49c11509af6510687254fb31
csly2023 <csly.2023@gmail.com>
2024-05-19 21:24:47 +0600

    card, slider dynamic - class 14

---
+++ views/detail.php
@@ -0,0 +1,22 @@
+<?php include "includes/header.php"; ?>
+<section class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-5">
+                <div class="card card-body">
+                    <img src="../../assets/img/s1.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-sm-7">
+                <div class="card card-body">
+                    <h1>This is Title</h1>
+                    <p>short description</p>
+                    <p>long description description</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include "includes/footer.php"; ?>
